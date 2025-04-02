585c604f479da85778aadccc32a1b8481adc919c
Sansals <stereotip.228@gmail.com>
2025-04-03 04:16:14 +0700

    Version 0.0.16 -Сырой вариант добавления пользовательских аватаров

--- work/login/models.py
+++ work/login/models.py
@@ -1,14 +1,24 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
+
+def user_avatar_directory(instance: '', filename: str) -> str:
+    return 'users/user_{pk}/avatars/{filename}'.format(
+        pk=instance.pk,
+        filename = filename,
+    )
 
 class User_Status(models.Model):
     user_id = models.AutoField (primary_key=True)
     username = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='login/avatars/', default='login/avatars/stive.jpg')
+    avatar = models.ImageField(upload_to=user_avatar_directory, default='login/avatars/stive.jpg')
     balance = models.IntegerField(default=0)
     Isverified = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("profile", kwargs={'url_username': self.username})
 
     def __str__(self):
         return f'{self.username}'
