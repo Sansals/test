6fff0201afe95c76c24399f58106feb86a78805f
Sansals <stereotip.228@gmail.com>
2025-04-05 04:42:10 +0700

    Version 0.0.20 -Начата разработка структуры форума, добавлены модели ForumQuestions, ForumAnswer

--- work/forum/admin.py
+++ work/forum/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Articles, Public_Chat
+from .models import Articles, Public_Chat, ForumQuestions, ForumAnswer
 
 admin.site.register(Articles)
 admin.site.register(Public_Chat)
-
-
+admin.site.register(ForumQuestions)
+admin.site.register(ForumAnswer)
 # Register your models here.
--- work/forum/models.py
+++ work/forum/models.py
@@ -39,4 +39,68 @@ class Articles(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
 
+
+class ForumQuestions(models.Model):
+    PROBLEMS_WITH_GAME_CLIENT = 'Проблемы с клиентом игры'
+    PROBLEMS_WITH_SITE = 'Проблемы с сайтом'
+    PROBLEMS_WITH_PAYMENT = 'Проблемы с оплатой'
+    GET_ANSWER_THE_QUESTION = 'Получить ответ на вопрос'
+    COMPLAINT_ABOUT_THE_USER = 'Сообщить о нарушении правил игроком'
+    APPEAL_THE_PUNISHMENT = 'Обжаловать наказание'
+    COMPLAINT_ABOUT_THE_ADMIN = 'Сообщить о нарушении правил со стороны Администрации'
+    COMPLAINT_ABOUT_THE_CLAN = 'Сообщить о нарушении правил кланом/группой игроков'
+
+    SUBJECT = {
+        (PROBLEMS_WITH_GAME_CLIENT, 'Проблемы с клиентом игры'),
+        (PROBLEMS_WITH_SITE, 'Проблемы с сайтом'),
+        (PROBLEMS_WITH_PAYMENT, 'Проблемы с оплатой'),
+        (GET_ANSWER_THE_QUESTION, 'Получить ответ на вопрос'),
+        (COMPLAINT_ABOUT_THE_USER, 'Сообщить о нарушении правил игроком'),
+        (APPEAL_THE_PUNISHMENT, 'Обжаловать наказание'),
+        (COMPLAINT_ABOUT_THE_ADMIN, 'Сообщить о нарушении правил со стороны Администрации'),
+        (COMPLAINT_ABOUT_THE_CLAN, 'Сообщить о нарушении правил кланом/группой игроков'),
+    }
+
+    subject = models.CharField('Раздел форума', max_length=52, choices= SUBJECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE,
+                                default=None,
+                                related_name='claimant',
+                                verbose_name = 'Заявитель'
+                                 )
+    defendant = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE,
+                                default=None,
+                                related_name = 'defendant',
+                                verbose_name = 'Ответчик'
+                                 )
+    question = models.TextField('Заявление', max_length= 600 )
+    proofs = models.CharField('Доказательства', max_length= 600)
+    date = models.DateTimeField(auto_now_add=True)
+    is_anonymous = models.BooleanField(default=False)
+    is_resolved = models.BooleanField(default=False)
+
+
+    class Meta:
+        verbose_name = 'Вопрос на форумe'
+        verbose_name_plural = 'Вопросы с форума'
+
+    def __str__(self):
+        return f'{self.subject} | {self.user.username} | {self.defendant.username} | {self.question}'
+
+
+class ForumAnswer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             default=None,
+                             related_name='user',
+                             verbose_name='Отвечающий'
+                             )
+    question = models.ForeignKey(ForumQuestions,
+                                verbose_name = 'Обращение',
+                               on_delete=models.CASCADE,
+                                 )
+    answer = models.TextField('Ответ', max_length= 600 )
+    is_anonymous = models.BooleanField(default=False)
+
 # Create your models here.
