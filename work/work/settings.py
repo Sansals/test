fbde10d4d4a9cbfffb41a4dd04c003a198a234bb
Sansals <stereotip.228@gmail.com>
2025-05-19 13:06:25 +0700

    Тест +12345

--- work/work/settings.py
+++ work/work/settings.py
@@ -194,7 +194,7 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Ares yilativ <stereotip.228@gmail.com>"
 
 # Default primary key field type
-#коммент для проверки гит
+#коммент для проверки гит 12345
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
